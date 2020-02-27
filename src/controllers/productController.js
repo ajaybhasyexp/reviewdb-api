7f426c8fdbec743c4979749be2f00078147085d7
Vishak <vpanthalath@acr.org>
2020-02-28 01:55:07 +0530

    productControl product query handler - fix

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -28,4 +28,12 @@ exports.getProductsById = async (req, reply) => {
     catch (error) {
         throw boom.boomify(error);
     }
+
+}
+exports.getProductByQuery = async (req, reply) => {
+    try{
+        return dataService.ProductService.getProduct(req.body)
+    }catch (error){
+        throw boom.boomify(error)
+    }
 }
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const routes = require('./routes');
 const mongoString = 'mongodb://fastifyuser:docker123@ds060649.mlab.com:60649/reviewdb';
 const swagger = require('./config/swagger');
 const path = require('path')
-const dataService = require(path.join(__dirname,'../../../reviewdb-data/index'));
+const dataService = require(path.join(__dirname,'../../reviewdb-data/index'));
 
 
 // Register Swagger
